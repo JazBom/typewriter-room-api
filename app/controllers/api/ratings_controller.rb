551e1d05e4c02b2afae8a15e6af4ad049fa669c2
Jessica Bommer <xininu@gmail.com>
2021-04-22 10:53:31 +1000

    add functions to text_item, inspiration and rating database controllers

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -1,19 +1,35 @@
-class Api:RatingsController < ApplicationController
+class Api::RatingsController < ApplicationController
   def index
+    render json: Rating.all, status: 200
   end
 
   def create
+    rating = Rating.create(rating_params)
+    if rating.valid?
+      render json: rating, status: 201
+    else
+      puts rating.errors.inspect
+      render json: { message: 'Unable to create rating' }, status: 500
+    end
   end
 
   def show
+    puts params[:id]
+    render json: Rating.find(params[:id]), status: 200
   end
 
   def update
+    puts params[:id]
+    render json: Rating.find(params[:id]).update(rating_params), status: 200
   end
 
   def destroy
+    puts params[:id]
+    Rating.destroy(params[:id])
+    render json: { message: 'Rating successfully destroyed.' }, status: 200
   end
 
-  def ratings_params
+  def rating_params
+    params.require(:rating).permit(:text_item_id, :rater_id, :rating)
   end
 end
--- app/controllers/api/text_items_controller.rb
+++ app/controllers/api/text_items_controller.rb
@@ -1,19 +1,35 @@
 class Api::TextItemsController < ApplicationController
   def index
+    render json: TextItem.all, status: 200
   end
 
   def create
+    text_item = TextItem.create(text_item_params)
+    if text_item.valid?
+      render json: text_item, status: 201
+    else
+      puts text_item.errors.inspect
+      render json: { message: 'Unable to create text item.' }, status: 500
+    end
   end
 
   def show
+    puts params[:id]
+    render json: TextItem.find(params[:id]), status: 200
   end
 
   def update
+    puts params[:id]
+    render json: TextItem.find(params[:id]).update(text_item_params), status: 200
   end
 
   def destroy
+    puts params[:id]
+    TextItem.destroy(params[:id])
+    render json: { message: 'Text item successfully deleted.' }, status: 200
   end
 
   def text_item_params
+    params.require(:text_item).permit(:text, :published, :writer_id, :inspiration_id)
   end
 end
