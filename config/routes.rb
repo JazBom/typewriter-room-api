eef2bfe1b41b2e1cc294c753e3240f43cc9cf442
Jessica Bommer <xininu@gmail.com>
2021-04-24 18:06:12 +1000

    add CRUD routes for API inspirations db and ratings db

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     post 'auth/login', to: 'auth#login'
     resources :users
     resources :text_items
+    resources :inspirations
+    resources :ratings
     
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
