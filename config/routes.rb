30b7845a3ef7d4e3a56b5d89aeb9452d4308b21b
Jessica Bommer <xininu@gmail.com>
2021-04-29 19:31:25 +1000

    add routes for published text items '/published', and routes for user-only text items '/my_items'

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     post 'auth/login', to: 'auth#login'
     resources :users
+    get 'text_items/my_items', to: 'text_items#my_items'
+    get 'text_items/published', to: 'text_items#published'
     resources :text_items
     resources :inspirations
     resources :ratings
-    
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
