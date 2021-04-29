223c382591069442f693a122ebd1504e2b7a55b1
Jessica Bommer <xininu@gmail.com>
2021-04-29 19:36:38 +1000

    create log-in exception to function checking for valid user (in Application controller)

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,11 +1,9 @@
 class Api::AuthController < ApplicationController
-    # before_action :user_valid, except: [:login]
+    skip_before_action :user_valid
     def login
       user = User.find_by(name: params[:name])
-      puts 'User'
-      p user
       if user&.authenticate(params[:password])
-        token = (encode_token user.id)
+        token = encode_token(user.id)
         render json: { user: user, token: token }
       else
         render json: { message: 'user not found' }, status: :not_found
