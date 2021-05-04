4adc3a5b2632508a5fa0b5d8871d1f08b3189dcd
Jessica Bommer <xininu@gmail.com>
2021-05-04 22:58:58 +1000

    add average rating method to Text_Item model to get average ratings for each text item

--- app/models/text_item.rb
+++ app/models/text_item.rb
@@ -2,4 +2,8 @@ class TextItem < ApplicationRecord
     belongs_to :inspiration
     belongs_to :writer, class_name: :User, foreign_key: :writer_id
     has_many :ratings
+
+    def avg_rating
+        self.ratings.average(:rating)
+    end
 end
