0b4eafd9c0f8f4b0663199352ac9c5e334183ccd
Jessica Bommer <xininu@gmail.com>
2021-05-08 12:57:05 +1000

    fix error saving empty text item, text item without inspo - add validation for text_item to check that title and inspiration_id exist

--- app/models/text_item.rb
+++ app/models/text_item.rb
@@ -2,7 +2,9 @@ class TextItem < ApplicationRecord
     belongs_to :inspiration
     belongs_to :writer, class_name: :User, foreign_key: :writer_id
     has_many :ratings
-
+    validates :title, presence: true, length: { minimum: 1 }
+    validates :inspiration_id, presence: true
+    #just added validates stuff
     def avg_rating
         self.ratings.average(:rating)
     end
