c8eb860be03f19352244ffbf4f3c324b0b33ade9
Jessica Bommer <xininu@gmail.com>
2021-05-08 10:55:57 +1000

    fix loophole allowing blank name user - add user validate method for presence and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :name, uniqueness: { message: 'User already exists' }
+    validates :name, uniqueness: { message: 'User already exists' }, presence: true, length: { minimum: 1 }
+   # just added - presence: true, length: { minimum: 1 } - as can currently save blank name
 end
